23d1a078f93ca5830656772115be0663f1c1dbd6
faust <160615653+Faustout@users.noreply.github.com>
2026-06-16 04:11:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const suits =['C', 'D', 'H', 'S', 'X']
 const values = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
 const jokerValues = ['B', 'R']
 let deck = []
+let playerCards = []
+let dealerCards = []
+announcementNode.textContent = "";
+let isLost = false
+let isWon = false
+let isTie = false
 
 function getDeck() {
     deck = []
@@ -52,6 +58,13 @@ function shuffleCards(deck){
          img.alt = `${card.suit}-${card.value}`
          img.className = 'card'
         document.querySelector(containerSelector).appendChild(img)
+
+        // store the cards in appropriate arrays
+        if (containerSelector === '#player-cards') {
+            playerCards.push(card)
+        } else if (containerSelector === '#dealer-cards'){
+            dealerCards.push(card)
+        }
 }
 
 
@@ -65,6 +78,8 @@ document.getElementById('new-game').addEventListener('click', () => {
 function clearCards() {
     document.getElementById('dealer-cards').innerHTML =''
     document.getElementById('player-cards').innerHTML =''
+    playerCards = []
+    dealerCards = []
 }
 
 function dealInitialHands(){
@@ -76,6 +91,14 @@ function dealInitialHands(){
     displayCard(deck.pop(), '#dealer-cards')
     displayCard(deck.pop(), '#dealer-cards')
 
+    // calculating and display scores
+    const dealerScore = countScore(dealerCards)
+    const playerScore = countScore(playerCards)
+
+    document.getElementById("dealer-number").textContent = dealerScore
+    document.getElementById("player-number").textContent = playerScore
+
+
     // show action button
     document.getElementById('hit-me').style.display = 'inline'
     document.getElementById('stay').style.display= 'inline'
@@ -83,10 +106,61 @@ function dealInitialHands(){
 }
 
 
+function countScore (card){
+    // for counting the total score
+
+    // checking if the card joker or not, joker = instant bust
+    let hasJoker = card.some(c => c.suit === "X")
+    if (hasJoker) {
+        return "Bust"
+    }
 
 
+    let hasAce = false
+    let score = card.reduce((total, card) =>{
+        if (card.value === "1"){
+            hasAce = true
+            return total = 1
+        }
+        if (['11','12','13'].includes(card.value)) {return total +10}
+        return total + Number(card.value)
+    },0 )
+    if (hasAce) {
+        score = (score + 10) > 21 ? score : score +10
+    }
+return score
+}
 
 
+function hitMe(target){
+
+    // for the player
+    if (target === "player") {
+        displayCard(deck.pop(), '#player-cards')
+        playerScore = countScore(playerScore)
+        document.getElementById("player-number").textContent = playerScore
+
+        if (playerScore > 21 || playerScore === "Bust"){
+        isLost = true
+        announcementNode.textContent = "You're out of the game!"
+        }
+    }
+
+    // for the dealer
+    if (target === "dealer"){
+        displayCard(deck.pop(), '#dealer-cards')
+        dealerPlays()
+    }
+}
+
+function dealerPlays (){
+    // if any of isLost / isWon / isTie becomes true, return immediately
+    // count the dealer's score with countScore() and update the UI to reflect the change
+
+    if (isLost || isTie || isWon) {return}
+    dealerScore = countScore(dealerScore)
+    document.getElementById("dealer-number").textContent = dealerScore
+}
 
 
 
